2d8999364ccf8feedefdb07ddda2398d7bf23b93
Guido Flohr <guido.flohr@cantanea.com>
2024-03-27 19:16:58 +0200

    make the hero editable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-heroes',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, FormsModule],
 	templateUrl: './heroes.component.html',
 	styleUrl: './heroes.component.css',
 })
