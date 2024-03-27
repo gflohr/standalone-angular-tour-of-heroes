2432de97b72f1747a4d5b85bccc52c1a32ed46fe
Guido Flohr <guido.flohr@cantanea.com>
2024-03-27 20:40:24 +0200

    add hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -12,8 +12,18 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 	styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-	heroes = HEROES;
+	heroes?: Hero[];
 	selectedHero?: Hero;
+
+	constructor(private heroService: HeroService) {}
+
+	ngOnInit() {
+		this.getHeroes()
+	}
+
+	private getHeroes() {
+		this.heroes = this.heroService.getHeroes();
+	}
 
 	onSelect(hero: Hero) {
 		this.selectedHero = hero;
