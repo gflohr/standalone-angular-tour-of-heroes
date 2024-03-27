a6a936e9f0ab8b464a2b7de7c545d52764342e93
Guido Flohr <guido.flohr@cantanea.com>
2024-03-27 19:24:05 +0200

    make hero editable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
 	selector: 'app-heroes',
 	standalone: true,
-	imports: [CommonModule, FormsModule],
+	imports: [CommonModule, FormsModule, NgFor],
 	templateUrl: './heroes.component.html',
 	styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm',
-	};
+	heroes = HEROES;
+	selectedHero?: Hero;
+
+	onSelect(hero: Hero) {
+		this.selectedHero = hero;
+	}
 }
