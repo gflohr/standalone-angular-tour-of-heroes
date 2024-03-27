1b40c6ff10a69d9b500d6534c2b34a3677dfa6f0
Guido Flohr <guido.flohr@cantanea.com>
2024-03-27 12:57:34 +0200

    import the CommonModule for uppercase pipe

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-heroes',
 	standalone: true,
-	imports: [],
+	imports: [CommonModule],
 	templateUrl: './heroes.component.html',
 	styleUrl: './heroes.component.css',
 })
