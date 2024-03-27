4b07639a34212188e2aa2c064e69a277c0fc14ab
Guido Flohr <guido.flohr@cantanea.com>
2024-03-27 11:22:57 +0200

    replace boilerplate markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
 	styleUrl: './app.component.css',
 })
 export class AppComponent {
-	title = 'standalone-angular-tour-of-heroes';
+	title = 'Standalone Tour of Heroes';
 }
