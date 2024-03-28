4a122d79d4dde01baaca68b283e97e809acd99c7
Guido Flohr <guido.flohr@cantanea.com>
2024-03-28 12:45:32 +0200

    get data over http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,33 @@ import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class HeroService {
-	constructor(private readonly messageService: MessageService) {}
+	private heroesUrl = 'api/heroes'; // URL to web API.
 
+	constructor(
+		private http: HttpClient,
+		private readonly messageService: MessageService,
+	) {}
+
+	/** GET heroes from the server */
 	getHeroes(): Observable<Hero[]> {
-		const heroes$ = of(HEROES);
-		this.messageService.add('HeroService: fetched heroes');
-		return heroes$;
+		return this.http.get<Hero[]>(this.heroesUrl);
 	}
-
 	getHero(id: number): Observable<Hero> {
 		// For now, assume that a hero with the specified `id` always exists.
 		// Error handling will be added in the next step of the tutorial.
 		const hero = HEROES.find(h => h.id === id)!;
-		this.messageService.add(`HeroService: fetched hero id=${id}`);
+		this.log(`HeroService: fetched hero id=${id}`);
 		return of(hero);
+	}
+
+	/** Log a HeroService message with the MessageService */
+	private log(message: string) {
+		this.messageService.add(`HeroService: ${message}`);
 	}
 }
