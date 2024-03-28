a0546574f5fc454e14e2e2ef4095d05297e5d4ab
Guido Flohr <guido.flohr@cantanea.com>
2024-03-28 11:17:17 +0200

    add routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [RouterOutlet, HeroesComponent, MessagesComponent],
+	imports: [RouterOutlet, RouterModule, HeroesComponent, MessagesComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [
-	{ path: 'heroes', component: HeroesComponent },
-];
+export const routes: Routes = [{ path: 'heroes', component: HeroesComponent }];
